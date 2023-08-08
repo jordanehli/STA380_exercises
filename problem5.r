7dff1737d5e19c7648534a2577f8f0cd96fe7ce7
Rafain Rodriguez San Millan <rafainrsm@Rafains-MacBook-Pro.local>
2023-08-08 15:27:29 -0500

    Add problem 2,3,5 and data files

---
+++ problem5.r
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(flexclust)
+
+wine_data <- read_csv('/Users/rafainrsm/src/summer23/STA380/STA380_exercises/data/wine.csv')
+summary(wine_data)
+
+View(wine_data)
+
+ggplot(wine_data) +  geom_point(aes(x=fixed.acidity, y=volatile.acidity, color=color))
+ggplot(wine_data) +  geom_point(aes(x=alcohol, y=quality, color=color))
+
+t <- wine_data %>% select(-color)
+cor_values <- cor(t)
+palette = colorRampPalette(c("green","white","red")) (20)
+heatmap(x = cor_values, col = palette, symm = TRUE)
+
+X = scale(t, center=TRUE, scale=TRUE)
+clust2 = kmeans(X, 2, nstart = 25)
+
+
+# Total sulfur dioxide vs Chlorides
+ggplot(wine_data) + geom_point(aes(x=total.sulfur.dioxide, y=chlorides, color=color))
+qplot(total.sulfur.dioxide, chlorides, data=wine_data, color=factor(clust2$cluster))
+
+# Fixed acidity vs Volatile acidity
+ggplot(wine_data) +  geom_point(aes(x=fixed.acidity, y=volatile.acidity, color=color))
+qplot(fixed.acidity, volatile.acidity, data=wine_data, color=factor(clust2$cluster))
+
+# Fixed acidity vs Density
+ggplot(wine_data) +  geom_point(aes(x=fixed.acidity, y=density, color=color))
+qplot(fixed.acidity, density, data=wine_data, color=factor(clust2$cluster))
+
+# Alcohol vs pH
+ggplot(wine_data) +  geom_point(aes(x=alcohol, y=pH, color=color))
+qplot(alcohol, pH, data=wine_data, color=factor(clust2$cluster))
+
+
+# template
+ggplot(wine_data) +  geom_point(aes(x=citric.acid, y=sulphates, color=color))
+qplot(citric.acid, sulphates, data=wine_data, color=factor(clust2$cluster))
